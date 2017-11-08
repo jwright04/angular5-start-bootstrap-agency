ce32cf4d3ccd7dc2928994dc575a24c468aeb2ba
James Wright <jcwright04@gmail.com>
2017-11-07 22:13:25 -0600

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Renderer2} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+  constructor(private renderer:Renderer2){
+
+  }
+
+  ngAfterViewInit(): void {
+    this.renderer.setAttribute(document.querySelector('body'), "id", "page-top");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HeaderComponent } from './header/header.component';
+import { ServicesComponent } from './services/services.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { AboutComponent } from './about/about.component';
+import { TeamComponent } from './team/team.component';
+import { ClientsComponent } from './clients/clients.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HeaderComponent,
+    ServicesComponent,
+    PortfolioComponent,
+    AboutComponent,
+    TeamComponent,
+    ClientsComponent,
+    ContactComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
